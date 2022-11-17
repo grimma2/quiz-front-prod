d61e0a5eeb389ac5584aa406d8444618ec283ddf
grimma2 <80467627+grimma2@users.noreply.github.com>
2022-11-17 20:47:20 +0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,4 @@
-const webpack = require('webpack')
 const { defineConfig } = require('@vue/cli-service')
-
 module.exports = defineConfig({
   transpileDependencies: true,
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    })
-  ]
 })
