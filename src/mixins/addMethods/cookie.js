accaaa55bc447e9db824c63a975c7b61ce3a37b7
grimma2 <80467627+grimma2@users.noreply.github.com>
2022-11-21 14:25:36 +0500

    change check cookie existing

--- src/mixins/addMethods/cookie.js
+++ src/mixins/addMethods/cookie.js
@@ -1,13 +1,7 @@
 export default {
   methods: {
     cookiesIsExists (name) {
-      let cookies = document.cookie
-
-      let firstCondition = cookies.includes(`; ${name}=`)
-      let secondCondition = cookies.match(/inGame=(.*); /)
-      console.log(`check cookie f: ${firstCondition} s: ${secondCondition}`)
-
-      if (firstCondition || secondCondition) return true
+      if (document.cookie.includes(`${name}=${content}`)) return true
     },
     removeCookie() {
       document.cookie = 'inGame=1; Max-Age=-99999999;'
