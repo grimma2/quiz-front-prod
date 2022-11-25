f1ade1b6b9b2acb83af2541671aed7d7e7a36a54
grimma2 <80467627+grimma2@users.noreply.github.com>
2022-11-25 15:57:29 +0500

    new build

--- src/api/defaults.js
+++ src/api/defaults.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 export const backendHost = '62.113.100.228:8000'
 
 export const ax = axios.create({
-  baseURL: `http://${backendHost}/api/v1/`
+  baseURL: `${location.protocol}//${backendHost}/api/v1/`
 })
 
 
