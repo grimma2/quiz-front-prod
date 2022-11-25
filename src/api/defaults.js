5824a96bdbaf38c571aa130c87c8e11d8d8d6a2f
grimma2 <80467627+grimma2@users.noreply.github.com>
2022-11-25 16:26:16 +0500

    .

--- src/api/defaults.js
+++ src/api/defaults.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-export const backendHost = '62.113.100.228:8000'
+export const backendHost = 'quiz-back-prod.herokuapp.com'
 
 export const ax = axios.create({
   baseURL: `${location.protocol}//${backendHost}/api/v1/`
