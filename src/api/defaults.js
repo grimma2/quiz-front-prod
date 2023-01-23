dbe2b50066e2cdb0659b052e1259fbbdcc776785
grimma2 <80467627+grimma2@users.noreply.github.com>
2023-01-23 10:12:03 +0500

    change to local work

--- src/api/defaults.js
+++ src/api/defaults.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
 
-export const backendHost = 'quiz-game1.ru'
+export const backendHost = 'localhost'
 
 export const ax = axios.create({
-  baseURL: `${location.protocol}//${backendHost}/api/v1/`,
+  baseURL: `http://${backendHost}/api/v1/`,
   withCredentials: true
 })
 
